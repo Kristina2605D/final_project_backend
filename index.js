0dbc55e243c4bb895d459215015da04deb672045
Kristina2605D <109177697+Kristina2605D@users.noreply.github.com>
2022-12-13 00:33:01 +0400

    connect to mongo db

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(barev);
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import config from './config.js';
+config()
+
+
+const app = express();
+const jsonparser = bodyParser.json()
+app.use (jsonparser);
+const PORT = 5000;
+app.get('/', (req, res) => {
+    res.send('barev')
+})
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
